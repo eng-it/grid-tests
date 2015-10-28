26e75e4bb3d7e40b0b81230abc0696b9dcf632ee
Jesse Connell <jesse08@bu.edu>
2015-10-28 16:07:07 -0400

    Lumerical MPI example

---
+++ mpi-lumerical/fdtd-mpi-eth.sh
@@ -0,0 +1,6 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe mpi 16
+engine="/ad/eng/opt/64/lumerical-fdtd/bin/fdtd-engine-ompi-lcl"
+datafile="paralleltest-eth.fsp"
+time mpirun --mca btl_tcp_if_include eth0 -np $NSLOTS "$engine" -logall "$datafile"
---
+++ mpi-lumerical/fdtd-mpi-ib.sh
@@ -0,0 +1,6 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe mpi 16
+engine="/ad/eng/opt/64/lumerical-fdtd/bin/fdtd-engine-ompi-lcl"
+datafile="paralleltest-ib.fsp"
+time mpirun -np $NSLOTS "$engine" -logall "$datafile"
