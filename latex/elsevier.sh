b801d873fd5a2ee7e422c4af86b1a314bc53dc86
Jesse Connell <jesse08@bu.edu>
2016-03-14 13:43:00 -0400

    latex rendering examples for Elsevier articles

---
+++ latex/elsevier.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Load the "tex" module first so that the third-party files (elsarticle.cls and
+# associated files in this case) can be found by the LaTeX commands.  pdflatex
+# is called twice below so that citations/labels will be tracked correctly.  The
+# output file will show a warning about halfway through about this, but the 
+# output from the second run below that should not.
+
+module load tex
+pdflatex $1
+pdflatex $1
