fa5d1b63bfca8ff730d05d7d366886ae22d5a19f
Jesse Connell <jesse08@bu.edu>
2016-03-14 13:51:35 -0400

    add a basic MPI test using a lumerical demo binary

---
+++ mpi-lumerical/fdtd-mpi-test.sh
@@ -0,0 +1,10 @@
+# A very basic test that uses a Lumerical binary that estimates the value of Pi.
+# This will not specify the network device to use, so if InfiniBand is present
+# and working, it should be used automatically.
+#
+#$ -cwd
+#$ -pe mpi 4
+hostname
+date
+engine="/ad/eng/opt/64/lumerical/mpitest/cpi-ompi-lcl"
+mpirun -np $NSLOTS "$engine"
