5a4607209e2d7fb60079ccd24820bf5be698aeab
Jesse Connell <jesse08@bu.edu>
2016-06-03 11:59:58 -0400

    gurobi: copy over matlab example from install dir

--- gurobi/gurobi_test_matlab.sh
+++ gurobi/gurobi_test_matlab.sh
@@ -3,5 +3,6 @@
 #$ -l s_vmem=4G
 
 module load gurobi
+cp -u $GUROBI_HOME/examples/matlab/mip1.m .
 export MATLAB_PREFDIR=/mnt/nokrb/$USER/MATLAB
-matlab86 -nodesktop -nosplash -r gurobi_test_matlab
+matlab86 -nodesktop -nosplash -r mip1
