312647e54d64d6bdc09971228ba7182776b54a79
Jesse Connell <jesse08@bu.edu>
2016-08-17 12:07:48 -0400

    qsub: add more working directory notes

--- qsub/qsub-test.sh
+++ qsub/qsub-test.sh
@@ -4,7 +4,6 @@
 # list in the SGE configuration will be qsub'd as login shells, with profile
 # scripts and such being sourced.  So, they'll get the usual path and module
 # settings.  Other shells will be non-login and more stripped-down.
-# login_shells is currently sh,bash,ksh,csh,tcsh .
 # 
 # This script looks weird because it's written to also function as a csh script.
 # 
@@ -12,7 +11,14 @@
 #$ -cwd
 #$ -l s_vmem=256M
 #$ -j y
+
+# Note the difference here between the script's location at $0 (some temporary
+# Grid-defined path) and the working directory shown by `pwd`.  Assuming jobs
+# are being run with the usual -cwd argument given, any other files needed by a
+# job script are probably relative to the current working directory rather than
+# the script path.
 echo SHELL: $SHELL
+echo Script: $0
 echo User: `whoami`
 echo date: `date`
 echo hostname: `hostname -f`
